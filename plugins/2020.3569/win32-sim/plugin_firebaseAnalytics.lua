77e834794b5e4c7e9e73722b584b931704d59f5c
Scott Harrison <scottrules44@gmail.com>
2022-04-29 00:32:36 -0400

    Update plugin_firebaseAnalytics.lua

--- plugins/2020.3569/win32-sim/plugin_firebaseAnalytics.lua
+++ plugins/2020.3569/win32-sim/plugin_firebaseAnalytics.lua
@@ -4,8 +4,16 @@ local Library = require "CoronaLibrary"
 local lib = Library:new{ name='plugin.firebaseAnalytics', publisherId='tech.scotth' }
 -- Default implementations
 local function defaultFunction()
-	print( "WARNING: The '" .. lib.name .. "' library is not available on this platform." )
-	print( "WARNING: this plugin outdated please use version from solar2dmarketplace.com." )
+	native.showAlert( "Warning", "You are using on unsupported plugin, please see updated plugin on solar2dmarketplace.com", { "Ignore", "View Updated Plugin" }, function(event)
+	  if ( event.action == "clicked" ) then
+	      local i = event.index
+	      if ( i == 1 ) then
+		  -- Do nothing; dialog will simply dismiss
+	      elseif ( i == 2 ) then
+		  system.openURL( "https://solar2dmarketplace.com/plugins?FirebaseAnalytics_tech-scotth" )
+	      end
+	  end
+	end )
 end
 
 lib.init = defaultFunction
